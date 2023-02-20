691477a7ddfc9ffa95a2784640e4504fedb49b05
heonrim <xian332927@gmail.com>
2023-02-20 11:53:49 +0800

    Initial Commit

---
+++ commHome/commHomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  commHomeApp.swift
+//  commHome
+//
+//  Created by heonrim on 2/20/23.
+//
+
+import SwiftUI
+
+@main
+struct commHomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
